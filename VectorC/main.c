c029f590760926ba0eba8d3372208c2cf3c04db7
Claire Rogers <claire@guiltydog.co.uk>
2024-12-29 19:40:10 +0000

    Initial Commit

---
+++ VectorC/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  VectorC
+//
+//  Created by Claire Rogers on 29/12/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
